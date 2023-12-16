afe0a94284ff049ec8c5ae307ca6195663c19c5b
iion74 <147688919+iion74@users.noreply.github.com>
2023-12-16 16:35:10 +0900

    Add files via upload

---
+++ 8.21 소스코드.py
@@ -0,0 +1,43 @@
+class TNode:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+def is_Balanced(root,isBalanced=True):
+
+    if root is None or not isBalanced:
+        return 0, isBalanced
+
+    left_height, isBalanced = is_Balanced(root.left, isBalanced)
+
+    right_height, isBalanced = is_Balanced(root.right, isBalanced)
+
+    if abs(left_height - right_height) >1 :
+        isBalanced = False
+
+    return max(left_height, right_height) + 1, isBalanced
+
+
+if __name__ == '__main__':
+
+    ''' Construct the following tree
+                  A
+                /   \
+              B       E
+             / \       \
+            C   D       F
+    '''
+
+    d = TNode('D',None,None)
+    f = TNode('F',None,None)
+    e = TNode('E', None, f)
+    c = TNode('C',None,None)
+    b = TNode('B', c, d)
+    root = TNode('A',b,e)
+
+
+    if is_Balanced(root):
+        print('Binary tree is balanced')
+    else:
+        print('Binary tree is not balanced')
